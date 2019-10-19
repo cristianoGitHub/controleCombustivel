9d81233f0d9ca9955c91a21ce547dff104763ae9
Karl Neuman <karl.publicidade@gmail.com>
2019-10-19 14:43:09 -0300

    Classe Gastos

---
+++ CombustivelControle/src/Gastos.java
@@ -0,0 +1,37 @@
+
+public class Gastos {
+
+	String nome;
+	float kmRodado;
+	float litros;
+	float autonomia;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getKmRodado() {
+		return kmRodado;
+	}
+	public void setKmRodado(float kmRodado) {
+		this.kmRodado = kmRodado;
+	}
+	public float getLitros() {
+		return litros;
+	}
+	public void setLitros(float litros) {
+		this.litros = litros;
+	}
+	public float getAutonomia() {
+		return autonomia;
+	}
+	public void setAutonomia(float autonomia) {
+		this.autonomia = autonomia;
+	}
+	
+	
+	
+	
+}
